8b47b1ca7693bb9e22c877cf59bb64bebc5a04ba
edisdev <hedis285@gmail.com>
2020-02-20 00:51:25 +0300

    Delete unnecessary files

--- src/utils/toast.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { useToasts } from 'react-toast-notifications'
-
-export default ({ message, appearance = 'success'}) => {
-  const { addToast } = useToasts(null)
-  addToast(message, appearance)
-  return <></>
-}
